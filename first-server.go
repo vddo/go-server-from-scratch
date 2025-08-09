bdcf5a71e13ac494c69b9e69df5a2d1b2d8c47e6
Viet Duc Do <v.do@aevoloop.com>
2025-08-09 13:18:37 +0200

    Add response

--- first-server.go
+++ first-server.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello, world\n")
+}
+
 func main() {
+	http.HandleFunc("/", helloHandler)
 	server := http.Server{
-		Addr:    ":8080",
-		Handler: nil,
+		Addr: ":8080",
 	}
 	server.ListenAndServe()
 }
